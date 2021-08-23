34339ea9c9b6c3c39f3c479b9f13f1c804b1ef74
Golovolastik <72813121+Golovolastik@users.noreply.github.com>
2021-08-23 13:39:57 +0300

    delete virtual enviroment

--- module_6/scraper/parsing.py
+++ module_6/scraper/parsing.py
@@ -1,11 +1,12 @@
- import requests
+import requests
 from lxml import html
- def get_info():
-     r = requests.get("https://auto.ru/moskva/cars/used/")
-     print(r.content)
-     tree = html.fromstring(r.content)
+ 
+def get_info():
+    r = requests.get("https://auto.ru/moskva/cars/used/")
+    print(r.content)
+    tree = html.fromstring(r.content)
 
-     title_xpath = tree
+    #title_xpath = tree
 
 if __name__ == '__main__':
     get_info()
