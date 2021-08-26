99a9cff2b91f92d02f040e8fce0b25bdca42cf6c
Golovolastik <72813121+Golovolastik@users.noreply.github.com>
2021-08-26 20:42:58 +0300

    parse one page

--- module_6/scraper/parsing.py
+++ module_6/scraper/parsing.py
@@ -3,10 +3,25 @@ from lxml import html
  
 def get_info():
     r = requests.get("https://auto.ru/moskva/cars/used/")
-    print(r.content)
     tree = html.fromstring(r.content)
 
-    #title_xpath = tree
+    titles = tree.xpath(
+        '//a[@class="Link ListingItemTitle__link"]//text()'
+    )
+
+    prices = [
+        price.replace(u'\xa0', ' ') 
+        for price in tree.xpath(
+            '//div[@class="ListingItemPrice-module__content"]//text()')
+    ]
+    
+    params = [
+        param.replace(u'\xa0', ' ').replace(u'\u2009', ' ')
+        for param in tree.xpath(
+            '//div[@class="ListingItemTechSummaryDesktop__cell"][1]//text()')
+    ][::2]
+    
+    print(params)
 
 if __name__ == '__main__':
     get_info()
